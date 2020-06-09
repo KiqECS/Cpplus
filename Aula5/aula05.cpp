bbe2be94c2a99bdfc6d4bb3142face82f64f0d91
KiqECS <kaiquehelper2019@gmail.com>
2020-06-09 10:44:09 -0300

    Creating def

---
+++ Aula5/aula05.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#define pi 3.1415
+#define creator cout << "KiqECS\n\n";
+int main(){
+    int idade, anonascimento, anoatual=2020;
+    string nome, localnascimento;
+    cout << pi << "\n";
+    creator
+    return 0;
+}
