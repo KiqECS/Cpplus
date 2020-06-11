fc8f123299bdcc8e9547412cccf800d7bfb77617
KiqECS <kaiquehelper2019@gmail.com>
2020-06-11 17:05:26 -0300

    Negative convert

---
+++ Aula7/aula07.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int n1=5000;
+
+    // n1=n1+2; // Or - * / %
+    // n1+=2; // Or - * / %
+    // n1*=-1; // Or / %
+    // n1++; // More ONE(1)
+    // n1--; // Less ONE(1)
+    
+    cout << n1 << "\n\n";
+    return 0;
+}
