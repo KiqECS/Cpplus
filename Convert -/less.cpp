1b5c934f94ffd65e08e9610bdb1c606a40449279
KiqECS <kaiquehelper2019@gmail.com>
2020-06-11 17:05:55 -0300

    Negative converse

---
+++ Convert -/less.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int num;
+
+    cout << "Digite o número a ser convertido: ";
+    cin >> num;
+    num*=-1;
+    cout << "O numero na forma negativa é: " << num << "\n\n";
+
+    return 0;
+}
