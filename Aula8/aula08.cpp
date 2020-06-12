a87911d9a8468271ec0c1a251e9709421c07b11d
KiqECS <kaiquehelper2019@gmail.com>
2020-06-12 14:12:36 -0300

    The same Convert, but better.

---
+++ Aula8/aula08.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int num = 0;
+
+    cout << "Digite o número a ser invertido: ";
+    cin >> num;
+    num=-num;
+    cout << "O número invetido é: " << num << "\n\n";
+
+
+    return 0;
+}
