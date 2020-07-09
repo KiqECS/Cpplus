126c13b0011301936e90a9653d186f56384569b1
KiqECS <kaiquehelper2019@gmail.com>
2020-07-09 12:04:52 -0300

    Do first

---
+++ Aula16/DoWhile.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int cont = 400;
+    do
+    {
+        cout << "Leastway one time - " << cont << "\n";
+    } while (cont < 400);
+    
+    cout << "-------Finalizado------------\n";
+    return 0;
+}
