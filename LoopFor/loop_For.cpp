0f041372e014c2dac4158fafe0dbba5957c70cb1
KiqECS <kaiquehelper2019@gmail.com>
2020-07-18 11:32:51 -0300

    Forrrr

--- LoopFor/loop_For.cpp
+++ LoopFor/loop_For.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main(){
 
-    int numim, numpar, num;
+    int numimp, numpar, num;
     
     for(int tmp; tmp <= 1000000; tmp++); // Gives to u more initialize time of the code
 
     cout << "Números\n";
-    for ( num = 0,numim = 1,numpar = 0; num <= 100; num++, numim+=2, numpar+=2 )
+    for ( num = 0,numimp = 1,numpar = 0; num <= 100; num++, numimp+=2, numpar+=2 )
     {
-        cout << "num " << num << " - im " << numim << " - par " << numpar << "\n";  
+        cout << "num " << num << " - imp " << numimp << " - par " << numpar << "\n";  
     }
     
     return 0;
