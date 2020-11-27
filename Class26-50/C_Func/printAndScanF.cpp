f597c7fa1ed474e7206b6a4d702bad37948413c4
Kiqcoffee <kaiquehelper2019@gmail.com>
2020-11-27 11:06:37 -0300

    Formas de edição

--- Class26-50/C_Func/printAndScanF.cpp
+++ Class26-50/C_Func/printAndScanF.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <stdio.h>
+#include <math.h>
 
 using namespace std;
 
 int main(){
-    int idade;
-    char nome[10];
+    //int idade;
+    //char nome[10];
+    float pi = M_PI;
 
-    printf("Digite o seu nome: ");
-    scanf("%s",&nome);
+    printf("Valor de PI: %f\n",pi);
+    printf("Valor de PI: %0.2f\n",pi); // Muda a quantidade de casas decimais
+    printf("Valor de PI: %07.2f\n",pi); // Nesse caso gera um numero com 7 casas onde não tem número se completa com 0
 
-    printf("Digite a sua idade: ");
-    scanf("%d",&idade);
+    
 
-    printf("Olá %s prazer em conhece-lo!\nVocê tem %d, é isso mesmo?\n",nome,idade);
+    /*
+        printf("Digite o seu nome: ");
+        scanf("%s",&nome);
+
+        printf("Digite a sua idade: ");
+        scanf("%d",&idade);
+
+        printf("Olá %s prazer em conhece-lo!\nVocê tem %d, é isso mesmo?\n",nome,idade);
+    */
     return 0;
 }
