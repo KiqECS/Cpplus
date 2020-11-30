e52f61188b97a4c149a928583ce7e464a3c11102
Kiqcoffee <kaiquehelper2019@gmail.com>
2020-11-30 04:53:53 -0300

    Comandos dos sistema

---
+++ Class26-50/System/system.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+
+    system("ls"); // Roda comandos dos meu sistema, nesse caso o Linux
+    system("cd /home/kiq/LearnWhatever/Cpplus/Class26-50/Queue && ls");
+    return 0;
+}
